6544589bad433558bc5340a4e03451b2b788f121
jibril04Afaa <128831088+jibril04Afaa@users.noreply.github.com>
2024-03-13 11:52:27 -0400

    Made progress with the previous() function

--- script.js
+++ script.js
@@ -1,6 +1,48 @@
 let nextBtn = document.querySelector(".next")
 let prevBtn = document.querySelector(".previous")
+let artContainer = document.querySelector(".art-piece-ctnr")
+ 
 
-nextBtn.addEventListener('click')
+let dreamOfSpring = document.querySelector(".dream-of-spring")
+let nearTheEnd = document.querySelector(".near-the-end")
+let grapeHarvest = document.querySelector(".grape-harvest")
+let thePsyche = document.querySelector(".the-psyche")
+let angelOfAnnun = document.querySelector(".angel-of-annun")
 
-prevBtn.addEventListener('click')
+let imgs = [dreamOfSpring, nearTheEnd, grapeHarvest, thePsyche, angelOfAnnun]
+let randomImg = imgs[Math.floor(Math.random() * imgs.length)]
+
+nextBtn.addEventListener('click', next)
+prevBtn.addEventListener('click', previous)
+
+
+
+// displays a random art piece
+function next() {
+    randomImg.style.display = 'none'
+    // randomIndex updates every time the next button is clicked
+    let randomIndex = Math.floor(Math.random() * imgs.length)
+    randomImg = imgs[randomIndex]
+    randomImg.style.display = 'block'
+
+    // stores the randomly appeared images in a list
+    let nextBtnImgs = []
+
+    // convert randomImg to a number, push the actual number to the nextBtnImgs array
+    nextBtnImgs.push(randomImg[randomIndex]) 
+    
+    
+}
+
+function previous() {
+    // set the most recent image to be the last image shown from the next() function
+    randomImg.style.display = 'none'
+    // show the images in reverse order
+    // let mostRecentImg = randomImg[imgs.length - 1]
+
+    // store the images shows when the next button is clicked in an array
+
+    // reverse that array
+    // display that array's contents when the previous() function is called
+
+}
